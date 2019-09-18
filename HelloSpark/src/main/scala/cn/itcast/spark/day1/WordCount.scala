2ca34c80d201c07b88e92fdd08d07d2c52615824
xiaochouzheng <37055868+xiaochouzheng@users.noreply.github.com>
2019-09-18 17:59:05 +0800

    Add files via upload

---
+++ HelloSpark/src/main/scala/cn/itcast/spark/day1/WordCount.scala
@@ -0,0 +1,18 @@
+package cn.itcast.spark.day1
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by root on 2016/5/14.
+  */
+object WordCount {
+  def main(args: Array[String]) {
+    //非常重要，是通向Spark集群的入口
+    val conf = new SparkConf().setAppName("WC")
+    val sc = new SparkContext(conf)
+
+    sc.textFile(args(0)).flatMap(_.split(" ")).map((_, 1))
+      .reduceByKey(_+_).sortBy(_._2, false).saveAsTextFile(args(1))
+    sc.stop()
+  }
+}
